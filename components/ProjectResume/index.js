2287a975af24306dbb4e8992c7cf9120c91cf3c6
lihsinchen <50491726+lihsinn@users.noreply.github.com>
2023-02-06 00:15:43 +0800

    second commit

--- components/ProjectResume/index.js
+++ components/ProjectResume/index.js
@@ -36,8 +36,10 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+const resumeLink ="https://github.com/lihsinn/portfolio/raw/main/data/resume.pdf";
+  // "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+
+
 
 const ProjectResume = () => {
   const [width, setWidth] = useState(1200);
